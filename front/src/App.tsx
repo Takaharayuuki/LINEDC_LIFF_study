f5f2f0263928c4f89d2ae3ac73175fb6d7e4aff8
Takaharayuuki <takahara.yuuki22@gmail.com>
2023-07-26 01:06:21 +0900

    add: shareTargetPicker sample

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,41 +1,82 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID
-      })
-      .then(() => {
-        setMessage("LIFF init succeeded.");
-      })
-      .catch((e: Error) => {
-        setMessage("LIFF init failed.");
-        setError(`${e}`);
-      });
+  const [formData, setFormData] = useState({
+    date: "",
+    location: "",
+    details: "",
   });
 
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const onClickShare = () => {
+    if (liff.isApiAvailable("shareTargetPicker")) {
+      const message: { type: "text"; text: string } = {
+        type: "text",
+        text: `イベントの詳細:\n日時: ${formData.date}\n場所: ${formData.location}\n詳細: ${formData.details} \n\n LIFF勉強会情報アカウント を友だち追加できます\nhttps://lin.ee/xoM6MwD`,
+      };
+
+      liff
+        .shareTargetPicker([message])
+        .catch(() => {
+          alert(ERROR_MESSAGE.SHARE_TARGET_PICKER);
+        })
+        .finally(() => {
+          liff.closeWindow();
+        });
+    } else {
+      alert(ERROR_MESSAGE.DEVICE_NOT_SUPPORTED);
+    }
+  };
+
+  const ERROR_MESSAGE = {
+    SHARE_TARGET_PICKER:
+      "シェアターゲットピッカーの起動に失敗しました。少し時間をおいてから再度お試しください。",
+    DEVICE_NOT_SUPPORTED:
+      "申し訳ありませんが、このデバイスでは「シェアターゲットピッカー」が利用できません。他の方法で共有をお試しください。",
+  };
+
   return (
-    <div className="App">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
-      )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
+    <div>
+      <h2>イベント招待</h2>
+      <div>
+        <label>
+          日時:
+          <input
+            type="datetime-local"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          場所:
+          <input
+            type="text"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          詳細:
+          <textarea
+            name="details"
+            value={formData.details}
+            onChange={handleChange}
+          />
+        </label>
+      </div>
+      <button onClick={onClickShare}>イベントを友達にシェアする</button>
     </div>
   );
 }
