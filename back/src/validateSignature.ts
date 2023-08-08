c212b300f094486174dd392048affe1eeedc924c
tokku5552 <physics.0121@gmail.com>
2023-08-08 23:34:54 +0900

    add: methods

---
+++ back/src/validateSignature.ts
@@ -0,0 +1,44 @@
+import { validateSignature } from '@line/bot-sdk';
+import crypto from 'crypto';
+
+const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET || '';
+
+/**
+ * LINE SDKを使って、signatureを検証する
+ * @param body
+ * @param signature
+ * @returns
+ */
+export const validateSignatureSDK = (
+  body: string,
+  signature: string
+): boolean => {
+  return validateSignature(body, LINE_CHANNEL_SECRET, signature);
+};
+
+/**
+ * signature生成
+ * @param body
+ * @param signature
+ * @returns
+ */
+export const generateSignature = (body: string): string => {
+  // https://developers.line.biz/ja/reference/messaging-api/#signature-validation
+  return crypto
+    .createHmac('SHA256', LINE_CHANNEL_SECRET)
+    .update(body)
+    .digest('base64');
+};
+
+/**
+ * 自前でsignatureを検証する
+ * @param body
+ * @param signature
+ * @returns
+ */
+export const validateSignatureLocal = (
+  body: string,
+  signature: string
+): boolean => {
+  return signature === generateSignature(body);
+};
