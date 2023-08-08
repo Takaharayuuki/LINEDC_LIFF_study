dee3ad918ee4b436484c00385e28974fb0aae972
tokku5552 <physics.0121@gmail.com>
2023-08-08 22:31:48 +0900

    add: express

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,4 +1,29 @@
-const main = () => {
-  console.log('Hello World');
-};
-export default main;
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+// CORSの許可
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+// body-parserに基づいた着信リクエストの解析
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const router = express.Router();
+
+router.post('/api/test', (req: Request, res: Response) => {
+  res.send(req.body);
+});
+app.use(router);
+
+// 3000番ポートでAPIサーバ起動
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
