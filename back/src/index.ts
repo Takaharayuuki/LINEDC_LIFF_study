ab3b7addf0154402826f561c58eb944f5ebb4c70
Takaharayuuki <takahara.yuuki22@gmail.com>
2023-08-11 17:08:27 +0900

    add: アンケート回答を受け取ってお礼を返すAPIを仮実装

--- back/src/index.ts
+++ back/src/index.ts
@@ -56,6 +56,20 @@ router.post('/webhook', async (req: Request, res: Response) => {
   res.status(200).send({ status: 'OK' });
 });
 
+// POST /post-answer
+interface MessageBody {
+  message: string;
+  id_token: string;
+}
+router.post('/post-answer', async (req: Request, res: Response) => {
+  // アンケートの回答結果を元にpushMessageを実施する
+  const body: MessageBody = req.body as MessageBody;
+  const userId = await verifyTokenAPI(body.id_token);
+  const sendMessage = `あなたは「${body.message}」だから参加してくれたんだね!ありがとう!`;
+  await pushMessage(userId, sendMessage);
+  res.status(200).send({ status: 'OK' });
+});
+
 app.use(router);
 app.listen(3000, () => {
   console.log('listening on port 3000!');
