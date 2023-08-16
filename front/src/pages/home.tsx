c45f2e43956d08b1d1c5f6355c3edc22269656ad
tokku5552 <physics.0121@gmail.com>
2023-08-17 00:33:12 +0900

    add: profile

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from "react";
 import liff from "@line/liff";
+import { useEffect, useState } from "react";
 
 function Home() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
+  const [profile, setProfile] = useState<{
+    userId: string;
+    displayName: string;
+    pictureUrl?: string;
+    statusMessage?: string;
+  }>();
+
   useEffect(() => {
     liff
       .init({
@@ -16,12 +23,29 @@ function Home() {
         setMessage("LIFF init failed.");
         setError(`${e}`);
       });
-  });
+    (async () => {
+      const currentProfile = await liff.getProfile();
+      console.log(currentProfile);
+      setProfile(currentProfile);
+    })();
+  }, [liff]);
 
   return (
     <div className="App">
       <h1>create-liff-app</h1>
       {message && <p>{message}</p>}
+      {profile && (
+        <>
+          <ul>
+            <li>userId: {profile.userId}</li>
+            <li>displayName: {profile.displayName}</li>
+            <li>
+              <img src={profile.pictureUrl} width={100} />
+            </li>
+            <li>statusMessage: {profile.statusMessage}</li>
+          </ul>
+        </>
+      )}
       {error && (
         <p>
           <code>{error}</code>
